2106c270f208807d99876a3bad8648520dc5b2ed
RonaldoMarquina <alberto.marquina.27@unsch.edu.pe>
2025-11-17 00:16:07 -0500

    chore: Clean up temporary folders after app setup

--- backend/core_temp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreTempConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core_temp'
--- backend/infrastructure_temp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InfrastructureTempConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'infrastructure_temp'
