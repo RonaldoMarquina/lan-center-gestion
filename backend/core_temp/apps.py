03dd15378096e6c00e4a468be70b798ad79b8c30
RonaldoMarquina <alberto.marquina.27@unsch.edu.pe>
2025-11-17 00:12:45 -0500

    build: Successfully initialize and run Django project

---
+++ backend/core_temp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CoreTempConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core_temp'
---
+++ backend/presentation_temp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PresentationTempConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'presentation_temp'
